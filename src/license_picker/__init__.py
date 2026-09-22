3ce92b92fdd8d60a5c8943eadf9d4983c2bbdb61
Radwan Abdulhadi Ahmed <127021904+rad03i2@users.noreply.github.com>
2026-09-22 10:03:23 +0300

    feat: expose public Python API

---
+++ src/license_picker/__init__.py
@@ -0,0 +1,6 @@
+"""Offline open-source license comparison helpers."""
+
+from .core import LICENSES, License, compare, get_license, recommend
+
+__all__ = ["LICENSES", "License", "compare", "get_license", "recommend"]
+__version__ = "1.0.0"
